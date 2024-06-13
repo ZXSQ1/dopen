49bba217e517aee1cb939b2ee51a404e461df8be
Me <me@example.com>
2024-06-13 14:40:05 +0200

    Fixed IsExists function

--- files/files.go
+++ files/files.go
@@ -15,3 +15,13 @@ return: true if it exists and false otherwise
 func IsExists(file string) bool {
 	return !os.IsNotExist(file)
 }
+
+func IsFile(file string) bool {
+	if !IsExists(file) {
+		return false
+	}
+
+	stat, _ := os.Stat(file)
+
+  return !stat.IsDir()
+}
