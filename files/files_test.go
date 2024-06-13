78566a515d740f102209029f0a354bf52e39584a
Me <me@example.com>
2024-06-14 00:21:19 +0200

    Rewrote Cleanup in TestGetFile function

--- files/files_test.go
+++ files/files_test.go
@@ -67,28 +67,18 @@ func TestGetFile(t *testing.T) {
 	tmpFileName := "tmp.d"
 
 	t.Cleanup(func() {
-		os.Remove(tmpFileName)
 		os.RemoveAll(tmpFileName)
 	})
 
-	// Case #1
 	os.Create(tmpFileName)
 	GetFile(tmpFileName)
 
-	// Case #2
 	os.Remove(tmpFileName)
 	GetFile(tmpFileName)
 
-	// Case #3
 	os.Remove(tmpFileName)
 	os.Mkdir(tmpFileName, 0644)
 	GetFile(tmpFileName)
-
-	/*
-		if it exits on case #3 with an error that goes like:
-			file <file> is a directory
-		then it is working properly
-	*/
 }
 
 func TestWriteFile(t *testing.T) {
