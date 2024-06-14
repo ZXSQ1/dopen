62c39a60ad418cebf917fec6f738f821653372a4
Me <me@example.com>
2024-06-14 17:46:15 +0200

    Wrote Test for GetEnvironVar function
    Works Properly

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,1 +1,21 @@
 package utils
+
+import (
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestGetEnvironVar(t *testing.T) {
+	variableName := "HOME"
+
+	out, _ := exec.Command("bash", "-c", "echo $"+variableName).Output()
+	expectedResult := strings.TrimSpace(string(out))
+	actualResult := GetEnvironVar(variableName)
+
+	println(expectedResult, actualResult)
+
+	if expectedResult != actualResult {
+		t.Fail()
+	}
+}
