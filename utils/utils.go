95f431afcfb8d17e4edbced45b494b8e36ce3c41
Me <me@example.com>
2024-06-14 17:35:54 +0200

    Added & Documented GetEnvironVar function

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,31 @@
 package utils
+
+import (
+	"os"
+	"strings"
+)
+
+/*
+description: gets the value of an environment variable
+arguments:
+
+	environVarName: the name of the variable to get the value of
+
+return:
+  - the value of the variable
+  - an empty string if there is no such variable
+*/
+func GetEnvironVar(environVarName string) string {
+	environVars := os.Environ()
+
+	for _, environVar := range environVars {
+		name := strings.Split(environVar, "=")[0]
+		value := strings.Split(environVar, "=")[1]
+
+		if name == environVarName {
+			return value
+		}
+	}
+
+	return ""
+}
