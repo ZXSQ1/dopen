947179ab3a9bd2d3f35732ed981f14dc3f676e22
Me <me@example.com>
2024-06-20 20:02:01 +0300

    Wrote & Documented GetArgParser function

--- argp/argp.go
+++ argp/argp.go
@@ -1,6 +1,18 @@
 package argp
 
 type ArgParser struct {
-	args           []string // arguments except arg[0]
+	args           []string // program arguments
 	optionHandlers map[string]func(...[]string) // option handlers
+}
+
+/*
+description: gets a new ArgParser object
+arguments:
+	args: a string slice of the program arguments
+return: an ArgParser object
+*/
+func GetArgParser(args []string) ArgParser {
+	return ArgParser{
+		args: args,
+	}
 }
