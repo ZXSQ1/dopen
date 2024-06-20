697069d91cc45a33e4393c526f4e7fd57085a6a7
Me <me@example.com>
2024-06-20 21:49:37 +0300

    Wrote Test for HandleArgs method
    Working properly

--- argp/argp_test.go
+++ argp/argp_test.go
@@ -14,3 +14,21 @@ func TestGetArgParser(t *testing.T) {
 	}
 }
 
+func TestArgParser_HandleArgs(t *testing.T) {
+	input := []string{"cp", "-p", "-r", "-P", "name", "type", "file/path"}
+	argParser := GetArgParser(input)
+
+	argParser.HandleArgs([]string{"-p", "-r"}, func(s ...string) {}, 0)
+	argParser.HandleArgs([]string{"-P"}, func(s ...string) {}, 2)
+
+	for _, val := range []string{"-p", "-r", "-P"} {
+		if _, found := argParser.optionHandlers[val]; !found {
+			t.Fail()
+		}
+	}
+
+	if argParser.optionArgLength["-p"] != 0 || argParser.optionArgLength["-P"] != 2 {
+		t.Fail()
+	}
+
+}
