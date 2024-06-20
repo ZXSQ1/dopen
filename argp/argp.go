3ec368dbf8312442e89c685803ad0cb1b722037d
Me <me@example.com>
2024-06-20 20:17:30 +0300

    Added optionArgLength field in ArgParser type

--- argp/argp.go
+++ argp/argp.go
@@ -3,14 +3,17 @@ package argp
 type OptionHandler func(...string)
 
 type ArgParser struct {
-	args           []string // program arguments
-	optionHandlers map[string]OptionHandler // option handlers
+	args            []string                 // program arguments
+	optionHandlers  map[string]OptionHandler // option handlers
+	optionArgLength map[string]uint          // a map of options and the number of their arguments
 }
 
 /*
 description: gets a new ArgParser object
 arguments:
+
 	args: a string slice of the program arguments
+
 return: an ArgParser object
 */
 func GetArgParser(args []string) ArgParser {
