d5faa885a6cdb0d7bdd3e5466aad29b25e3728ef
Me <me@example.com>
2024-06-23 00:29:37 +0300

    Wrote ListDocs function

--- viewer/list.go
+++ viewer/list.go
@@ -1,1 +1,32 @@
 package viewer
+
+import (
+	"os/exec"
+	"strings"
+)
+
+const (
+	docINSTALLED = 0
+	docNOTINSTALLED = 1
+)
+
+func ListDocs() map[string]int {
+	result := map[string]int{}
+	proc := exec.Command("dedoc", "list")
+	out, _ := proc.Output()
+
+	for _, doc := range strings.Split(string(out), ",") {
+		doc = strings.TrimSpace(doc)
+		docParts := strings.Split(doc, " ")
+
+		if len(docParts) > 1 {
+			doc = docParts[0]
+
+			result[doc] = docINSTALLED
+		} else {
+			result[doc] = docNOTINSTALLED
+		}
+	}
+
+	return result
+}
