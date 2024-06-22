3725331fe68db29f8cf5ae201b680febd3f39a51
Me <me@example.com>
2024-06-23 01:02:59 +0300

    Finalizing Program
    Only need one method

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/ZXSQ1/devdocs-tui/argp"
+	"github.com/ZXSQ1/devdocs-tui/doc_manager"
+	"github.com/ZXSQ1/devdocs-tui/files"
+)
+
+func help() {
+	message := `
+		syntax: <program> <language-doc-name>
+		options:
+			--help, -h 		for help
+	`
+
+	println(message)
+	os.Exit(1)
+}
+
+func main() {
+	argv := os.Args
+
+	if len(argv) < 2 {
+		help()
+	} else {
+		argParser := argp.GetArgParser(argv)
+
+		argParser.HandleArgs([]string{"-h", "--help"}, func(s ...string) { help() }, 0)
+		args := argParser.Execute()
+
+		for _, language := range args {
+			docManager := doc_manager.GetDocsManager(language)
+
+			if !files.IsExists(docManager.DocFile) {
+				docManager.FetchDocs()
+				docManager.FilterDocs()
+			}
+
+			// open doc function
+		}
+	}
 }
