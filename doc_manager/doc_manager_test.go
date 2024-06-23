ff38850594b95c6cb49eca56900662a6996feb3b
Me <me@example.com>
2024-06-23 22:42:49 +0300

    Wrote test for Init function (doc_manager_test)

--- doc_manager/doc_manager_test.go
+++ doc_manager/doc_manager_test.go
@@ -1,11 +1,39 @@
 package doc_manager
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/ZXSQ1/devdocs-tui/files"
+)
 
 func TestGetLanguageDir(t *testing.T) {
 	language := "go"
 
 	if GetLanguageDir("go") != rootDir+"/"+language {
 		t.Fail()
+	}
+}
+
+func TestInit(t *testing.T) {
+	t.Cleanup(func() {
+		os.RemoveAll(rootDir)
+	})
+
+	language := "rust"
+	paths := []string{rootDir, tempDir, rootDir + "/" + language}
+
+	for _, path := range paths {
+		if files.IsExists(path) {
+			os.RemoveAll(path)
+		}
+
+		Init(language)
+
+		if !files.IsExists(path) {
+			t.Fail()
+		}
+
+		Init(language)
 	}
 }
