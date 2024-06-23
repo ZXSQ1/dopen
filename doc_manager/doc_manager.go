483129a1145b833bbb48360d6a57a6629708343b
Me <me@example.com>
2024-06-23 19:42:06 +0300

    Defined Variables & Constants

--- doc_manager/doc_manager.go
+++ doc_manager/doc_manager.go
@@ -1,2 +1,16 @@
 package docmanager
 
+import "github.com/ZXSQ1/devdocs-tui/utils"
+
+const (
+	rootDirName = "dopen"
+
+	asyncExt = ".async"
+	rawExt   = ".raw"
+	indexExt = ".index"
+)
+
+var (
+	rootDir = utils.GetEnvironVar("HOME") + "/.cache/" + rootDirName
+	tempDir = rootDir + "/.temp"
+)
