af8484b21097149795ebf18de9bd11090b3f0aec
Me <me@example.com>
2024-06-29 21:34:38 +0300

    Wrote handle function (main) with all of the option handling

--- main/main.go
+++ main/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strings"
+	"unicode"
 
+	"github.com/ZXSQ1/dopen/doc_manager"
 	"github.com/ZXSQ1/dopen/utils"
 )
 
@@ -24,6 +27,59 @@ options:
 	os.Exit(1)
 }
 
+func handle(args []string) {
+	option := args[0]
+
+	switch option {
+	case "-h", "--help":
+		help()
+	case "-w", "--width":
+		if len(args) < 2 {
+			println("Error: no value specified after option")
+			help()
+		}
+
+		for _, char := range args[1] {
+			if !unicode.IsDigit(char) {
+				println("Error: value specified after option not a number")
+				help()
+			}
+		}
+
+		if len(args) < 3 {
+			println("Error: no language specified to open docs for")
+			help()
+		}
+
+		doc_manager.OpenDocs(args[2])
+
+	case "-l", "--list":
+		docList := doc_manager.ListDocs()
+
+		for index, docName := range docList[0] {
+			installedOrNot := docList[1][index]
+
+			if installedOrNot == "n" {
+				installedOrNot = "not installed"
+			} else {
+				installedOrNot = "installed"
+			}
+
+			fmt.Printf("%30s%30s\n", docName, installedOrNot)
+		}
+	case "-d", "--download":
+		if len(args) < 2 {
+			println("Error: no value specified after option")
+			help()
+		}
+
+		doc_manager.DownloadDocs(args[1])
+	default:
+		fmt.Printf("Error: option: %s not recognized\n", option)
+		help()
+	}
+} 
+
 func start(args []string) {
 	if len(args) < 2 {
 		help()
@@ -34,6 +90,7 @@ func start(args []string) {
 			help()
 		}
 
+		handle(args)
 	}
 }
 
