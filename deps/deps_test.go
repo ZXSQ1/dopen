eb2784d3555ed357e36194df88ba719f2bdd2fce
Me <me@example.com>
2024-06-30 01:11:37 +0300

    Wrote test for GetPkg function (deps)

--- deps/deps_test.go
+++ deps/deps_test.go
@@ -1,2 +1,25 @@
 package deps
 
+import (
+	"os"
+	"testing"
+
+	"github.com/ZXSQ1/dopen/files"
+)
+
+func TestGetPkg(t *testing.T) {
+	const (
+		url     = "https://www.gnu.org/software/hello/manual/hello.txt"
+		outFile = "f"
+	)
+
+	t.Cleanup(func() {
+		os.Remove(outFile)
+	})
+
+	GetPkg(outFile, url)
+
+	if out, _ := files.ReadFile(outFile); len(out) < 2000 {
+		t.Fail()
+	}
+}
