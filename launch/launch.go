83399bdbc6dcee76ab777adfdbffbcb998549459
Me <me@example.com>
2024-06-29 23:49:56 +0300

    Added function Fzf (launch)

--- launch/launch.go
+++ launch/launch.go
@@ -1,1 +1,25 @@
 package launch
+
+import (
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/ZXSQ1/dopen/utils"
+)
+
+func Fzf(writer io.Writer, reader io.Reader) {
+	fzfOptions := []string{"--layout=reverse"}
+
+	if fzfDefaultOptions := utils.GetEnvironVar("FZF_DEFAULT_OPTS"); fzfDefaultOptions != "" {
+		fzfOptions = strings.Split(fzfDefaultOptions, " ")
+	}
+
+	proc := exec.Command("fzf", fzfOptions...)
+	proc.Stdin = reader
+	proc.Stdout = writer
+	proc.Stderr = os.Stderr
+
+	proc.Run()
+}
