0890648d3f5362724e1b42bc060b80d275a58956
Me <me@example.com>
2024-06-30 01:00:06 +0300

    Added GetPkg function (deps)

--- deps/deps.go
+++ deps/deps.go
@@ -1,2 +1,38 @@
 package deps
 
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/ZXSQ1/dopen/files"
+)
+
+func GetPkg(dirPath, pkgURL string, pkgBin string) {
+	response, err := http.Get(pkgURL + "/" + pkgBin)
+
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	defer response.Body.Close()
+
+	file, _ := files.GetFile(dirPath + "/" + pkgBin)
+	defer file.Close()
+
+	for {
+		buffer := make([]byte, 5096)
+		readBytes, err := response.Body.Read(buffer)
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			log.Fatalln(err.Error())
+		}
+
+		file.Write(buffer[:readBytes])
+	}
+
+}
