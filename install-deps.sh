bde859650bf61b737ea95ce93afc9732eb1d69ff
Me <me@example.com>
2024-06-30 01:42:58 +0300

    Finished install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -9,21 +9,23 @@ function get-dedoc {
     URL="https://github.com/toiletbril/dedoc/releases/download/$VERSION"
 
     for bin in dedoc dedoc.exe; do
-        wget -P "$DEPS_ROOT" "$URL"/$bin
+        wget -nc -P "$DEPS_ROOT" "$URL"/$bin
     done
 }
 
 function get-fzf {
     VERSION=0.53.0
-    URL="https://github.com/junegunn/fzf/releases/download/$VERSION/"
+    URL="https://github.com/junegunn/fzf/releases/download/$VERSION"
 
     for bin in fzf-$VERSION-linux_amd64.tar.gz fzf-$VERSION-windows_amd64.zip; do
-        wget -P "$DEPS_ROOT" "$URL"/$bin
+        wget -nc -P "$DEPS_ROOT" "$URL"/$bin
 
         cd "$DEPS_ROOT"
 
         tar -xf "$bin"
         unzip "$bin"
+
+        rm "$bin"
 
         cd -
     done
@@ -31,15 +33,21 @@ function get-fzf {
 
 function get-ov {
     VERSION=0.35.0
-    URL="https://github.com/noborus/ov/releases/download/$VERSION/"
+    URL="https://github.com/noborus/ov/releases/download/v$VERSION"
 
     for bin in ov_"$VERSION"_linux_amd64.zip ov_"$VERSION"_windows_amd64.zip; do
-        wget -P "$DEPS_ROOT" "$URL"/$bin
+        wget -nc -P "$DEPS_ROOT" "$URL"/$bin
 
         cd "$DEPS_ROOT"
 
         unzip "$bin"
 
+        rm "$bin"
+
         cd -
     done
 }
+
+get-dedoc
+get-fzf
+get-ov
